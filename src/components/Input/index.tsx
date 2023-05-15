1e65c9d5a859adf0e07ca84a3394ef432aefb78e
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-05-15 14:23:24 -0300

    feat: improving toggle password button

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import { Eye, EyeSlash } from "@phosphor-icons/react";
 import "./styles.scss";
-import { ReactNode, useState } from "react";
+import { ReactNode, useCallback, useMemo, useState } from "react";
 
 interface inputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -12,20 +12,23 @@ interface inputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 export function Input(props: inputProps) {
   const { label, required, type, icon, name } = props;
   const [ isVisible, setIsVisible ] = useState(false)
+  const inputType = useMemo(() => (type === "password" ? ( isVisible ? "text" : "password" ) : type), [isVisible]) 
 
-  function togglePassword() {
+  const togglePassword = useCallback(() => {
     setIsVisible(!isVisible)
-  }
+  }, [ isVisible ])
+
+
 
   return (
     <div className="inputContainer">
       {icon}
-      <input {...props} type={type === "password" ? ( isVisible ? "text" : "password" ) : type} name={name} id={name} placeholder={label} required={required} />
-      { type === "password" && (
+      <input {...props} type={inputType} name={name} id={name} placeholder={label} required={required} />
+      {type === "password" && (
         <button className="toggleButton" onClick={togglePassword} type="button">
           { isVisible ? <EyeSlash size={20} /> : <Eye size={20} />}
         </button>
-      ) }
+      )}
     </div>
   );
 }
